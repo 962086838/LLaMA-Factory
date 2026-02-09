29f2e8996eabb87e816ea31886e5dadc94d34bd1
Haowei He <962086838@qq.com>
2026-02-09 14:42:06 +0800

    version check disabled

--- src/train.py
+++ src/train.py
@@ -11,6 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import os
+os.environ["DISABLE_VERSION_CHECK"] = "1"
 
 from llamafactory.train.tuner import run_exp
 
